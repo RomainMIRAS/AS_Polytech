057e158b466699ae50633aeed45ce8b725401e13
Axel.Colé <axel.cole@etu.univ-grenoble-alpes.fr>
2024-01-31 11:15:20 +0100

    semctl update

--- src/kernel/sys/sem/semctl.c
+++ src/kernel/sys/sem/semctl.c
@@ -1,20 +1,20 @@
 #include <../include/sys/sem.h>
-#include <sys/sem.c>
 #include <sys/sem/semget.c>
 
 PUBLIC int semctl(int semid, int cmd, int val)
 {
-    struct semaphore sem = semTab[sys_semget(semid)];
+    struct semaphore sem = semTab[semid];
     switch (cmd)
     {
     case GETVAL:
-        return sem;
+        return sem.value;
     case SETVAL:
         sem.semid = val;
         return 1;
     case IPC_RMID:
         sem.semid = -1;
-        return 0;
+        sem.chain = NULL;
+        return 1;
     default:
         return -1;
     }
