4401b9c1652c7aa4584484c374a96cf8618ef28b
Axel.Colé <axel.cole@etu.univ-grenoble-alpes.fr>
2024-01-31 10:53:37 +0100

    semctl0.1

--- src/kernel/sys/sem/semctl.c
+++ src/kernel/sys/sem/semctl.c
@@ -1,6 +1,20 @@
-#include <sys/sem.h>
+#include <../include/sys/sem.h>
+#include <sys/sem.c>
 
-PUBLIC int sys_semctl( int semid, int semnum, int cmd, ... )
+PUBLIC int sys_semctl(int semid, int cmd, int val)
 {
 
+    struct semaphore sem = semget(semid);
+    switch (cmd)
+    {
+    case GETVAL:
+        return sem;
+    case SETVAL:
+        sem.semid = val;
+        return 1;
+    case IPC_RMID:
+        return destroy(semid);
+    default:
+        return -1;
+    }
 }
