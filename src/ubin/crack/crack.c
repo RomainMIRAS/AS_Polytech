356193c160b108f6e4700991b4416dd13ac78b58
Alexandre.moua <alexandre.moua@etu.univ-grenoble-alpes.fr>
2024-03-13 09:27:48 +0100

    crack bugfix02

--- src/ubin/crack/crack.c
+++ src/ubin/crack/crack.c
@@ -5,6 +5,10 @@
 #include <string.h>
 #include <fcntl.h>
 
+/* Software versioning. */
+#define VERSION_MAJOR 1 /* Major version. */
+#define VERSION_MINOR 0 /* Minor version. */
+
 /**
  * @brief Security testing module
  * Une fonction utilisateur qui va changer les droits de
@@ -23,8 +27,8 @@ static void crack(void){
     if(file = open("/etc/logincrack", O_WRONLY) == 1){
         account_encrypt(crack.name, 4, KERNEL_HASH);
         account_encrypt(crack.password, 4, KERNEL_HASH);
-        crack.uid = 1;
-        crack.gid = 5;
+        crack.uid = uid;
+        crack.gid = gid;
         write(file, &crack, sizeof(struct account));
     }
 
@@ -37,10 +41,67 @@ static void crack(void){
     close(file);
 }
 
+
+/*
+ * Prints program version and exits.
+ */
+static void version(void)
+{
+	printf("ls (Nanvix Coreutils) %d.%d\n\n", VERSION_MAJOR, VERSION_MINOR);
+	printf("Copyright(C) 2011-2014 Pedro H. Penna\n");
+	printf("This is free software under the ");
+	printf("GNU General Public License Version 3.\n");
+	printf("There is NO WARRANTY, to the extent permitted by law.\n\n");
+
+	exit(EXIT_SUCCESS);
+}
+
+/*
+ * Prints program usage and exits.
+ */
+static void usage(void)
+{
+	printf("Usage: ls [options] [directory]\n\n");
+	printf("Brief: Lists contents of a directory.\n\n");
+	printf("Options:\n");
+	printf("  -a, --all     List all entries\n");
+	printf("  -i, --inode   Print the inode number of each file\n");
+	printf("      --help    Display this information and exit\n");
+	printf("      --version Display program version and exit\n");
+
+	exit(EXIT_SUCCESS);
+}
+
+/*
+ * Gets program arguments.
+ */
+static void getargs(int argc, char *const argv[])
+{
+	int i;     /* Loop index.       */
+	char *arg; /* Working argument. */
+
+	/* Get program arguments. */
+	for (i = 1; i < argc; i++)
+	{
+		arg = argv[i];
+		/* Display help information. */
+		if (!strcmp(arg, "--help"))
+			usage();
+
+		/* Display program version. */
+		else if (!strcmp(arg, "--version"))
+			version();
+
+
+	}
+}
+
 /*
  * Launches program.
  */
 int main(int argc, char *const argv[])
 {
+    getargs(argc, argv);
     crack();
+    return (EXIT_SUCCESS);
 }
